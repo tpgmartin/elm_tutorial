3b93e37f54d5769a924ee4aa6085ff4bf556226e
Tom Martin <tom@tpgmartin.com>
2016-09-03 14:39:49 +0100

    Create player models and route to app

--- elm_practice/src/Main.elm
+++ elm_practice/src/Main.elm
@@ -2,7 +2,7 @@ module Main exposing (..)
 
 import Html.App
 import Messages exposing (Msg)
-import Models exposing (Model)
+import Models exposing (Model, initialModel)
 import View exposing (view)
 import Update exposing (update)
 
@@ -10,7 +10,7 @@ import Update exposing (update)
 
 init : ( Model, Cmd Msg )
 init = 
-  ( "Hello", Cmd.none )
+  ( initialModel, Cmd.none )
 
 -- SUBSCRIPTIONS
 
